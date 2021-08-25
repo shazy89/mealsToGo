e00e89940d9c90705819a6f6b069aff518dd9b86
shazy89 <erdo.shazy123@gmail.com>
2021-08-25 16:48:51 -0400

    flexbox

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="dark" />
+        <View style={styles.box1}>
+          <Text>Box 1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text>Box 2</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-start"
   },
+  box1: {
+    flex: 0.1,
+    backgroundColor: "green"
+  },
+  box2: {
+    flex: 1,
+    backgroundColor: "red"
+  }
 });
