162fd1e38a63c5b6880ac6a2baa0af33e2162e52
shazy89 <erdo.shazy123@gmail.com>
2021-08-27 13:29:01 -0400

    Spacer component

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -20,15 +20,54 @@ const BottomMedium = styled(View)`
 const BottomLarge = styled(View)`
   margin-bottom: 16px;
 `;
+const LeftSmall = styled(View)`
+  margin-bottom: 4px;
+`;
+const LeftMedium = styled(View)`
+  margin-bottom: 8px;
+`;
+const LeftLarge = styled(View)`
+  margin-bottom: 16px;
+`;
+const RightSmall = styled(View)`
+  margin-bottom: 4px;
+`;
+const RightMedium = styled(View)`
+  margin-bottom: 8px;
+`;
+const RightLarge = styled(View)`
+  margin-bottom: 16px;
+`;
 
 const Spacer = ({ variant }) => {
-  if (variant === "top.medium") {
-    return <TopMedium />;
+  switch (variant) {
+    case "top.medium":
+      return <TopMedium />;
+    case "top.medium":
+      return <TopLarge />;
+    case "top.large":
+      return <TopMedium />;
+    case "bottom.small":
+      return <BottomSmall />;
+    case "bottom.medium":
+      return <BottomMedium />;
+    case "bottom.large":
+      return <BottomLarge />;
+    case "left.small":
+      return <LeftSmall />;
+    case "left.medium":
+      return <LeftMedium />;
+    case "left.large":
+      return <LeftLarge />;
+    case "Right.small":
+      return <RightSmall />;
+    case "Right.medium":
+      return <RightMedium />;
+    case "Right.large":
+      return <RightLarge />;
+    default:
+      <TopSmall />;
   }
-  if (variant === "top.large") {
-    return <TopLarge />;
-  }
-  return <TopSmall />;
 };
 
 export default Spacer;
